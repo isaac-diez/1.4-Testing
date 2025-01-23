821cc509b674308afd9b2f6089d51c8ea6eb7db3
isaac.diez <isaac.diez@proton.me>
2025-01-23 19:59:11 +0100

    feat: Level 2 Exercise 4. Add ArrayList with mixed object types and implement assertions
    - Created an ArrayList containing various types of objects (e.g., String, Integer, custom objects).
    - Added an assertion to verify the order of objects in the ArrayList as they were inserted.
    - Implemented a test to confirm that the list contains objects in any order.
    - Verified that one of the objects is added only once to the list.
    - Excluded one object from being added and confirmed that the list does not contain it.

--- src/Level2/main/java/org/level2Exer4/Model/Book.java
+++ src/Level2/main/java/org/level2Exer4/Model/Book.java
@@ -1,5 +1,7 @@
 package org.level2Exer4.Model;
 
+import java.util.Objects;
+
 public class Book {
 
     private String title;
@@ -9,4 +11,26 @@ public class Book {
         this.title = title;
         this.year = year;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return title.equals(book.title) && year == (book.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, year);
+    }
+
 }
--- src/Level2/test/java/org/level2Exer4/Model/TestArrayList.java
+++ src/Level2/test/java/org/level2Exer4/Model/TestArrayList.java
@@ -5,47 +5,87 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestArrayList {
 
+    private ArrayList<Object> listOfObjects;
+    private String string;
+    private int integer;
+    private Double decimal;
+    private boolean yesNo;
+    private Book book;
+
+
     @BeforeEach
     public void setup() {
 
+        listOfObjects = new ArrayList<>();
 
+        string = "person";
+        integer = 20;
+        decimal = 3.14;
+        yesNo = true;
+        book = new Book("The Great Gatsby", 1925);
+
+        listOfObjects.add(string);
+        listOfObjects.add(integer);
+        listOfObjects.add(decimal);
+        listOfObjects.add(yesNo);
+        listOfObjects.add(book);
 
     }
 
     @Test
     public void testArrayListElementsInAnyOrder() {
 
-        ArrayList<Book> listOfBooks = new ArrayList<>();
+        assertThat(listOfObjects).containsExactlyInAnyOrder(integer,string,yesNo,book,decimal);
 
-        Book book1 = new Book("To Kill a Mockingbird", 1960);
-        Book book2 = new Book("1984", 1949);
-        Book book3 = new Book("The Great Gatsby", 1925);
-        Book book4 = new Book("Pride and Prejudice", 1813);
-        Book book5 = new Book("The Catcher in the Rye", 1951);
+    }
 
-        listOfBooks.add(book1);
-        listOfBooks.add(book2);
-        listOfBooks.add(book3);
-        listOfBooks.add(book4);
-        listOfBooks.add(book5);
+    @Test
+    public void testArrayListElementsInCreationOrder() {
 
+        assertThat(listOfObjects).containsExactly(string,integer,decimal,yesNo,book);
 
+    }
+
+    @Test
+    public void testArrayListElementsOfInAnyOrder() {
+
+        ArrayList<Object> expectedElementsOfList = new ArrayList<>();
+
+        expectedElementsOfList.add(book);
+        expectedElementsOfList.add(string);
+        expectedElementsOfList.add(integer);
+        expectedElementsOfList.add(yesNo);
+        expectedElementsOfList.add(decimal);
+
+        assertThat(listOfObjects).containsExactlyInAnyOrderElementsOf(expectedElementsOfList);
 
 
     }
 
     @Test
-    public void testArrayListElementsOneIsMissing() {
+    public void testArrayListElementIsOnlyFoundOnce() {
 
-        Book book1 = new Book("To Kill a Mockingbird", 1960);
-        Book book2 = new Book("1984", 1949);
-        Book book3 = new Book("The Great Gatsby", 1925);
-        Book book4 = new Book("Pride and Prejudice", 1813);
-        Book book5 = new Book("The Catcher in the Rye", 1951);
+        assertThat(listOfObjects).containsOnlyOnce(book);
+        assertThat(listOfObjects).containsOnlyOnce(string);
+
+    }
+
+    @Test
+    public void testArrayListElementNotFound() {
+
+        ArrayList<Object> listOfOtherElements = new ArrayList<>();
+
+        listOfOtherElements.add(book);
+        listOfOtherElements.add(string);
+        listOfOtherElements.add(integer);
+        listOfOtherElements.add(yesNo);
+
+        assertThat(listOfOtherElements).doesNotContain(decimal);
 
     }
 
