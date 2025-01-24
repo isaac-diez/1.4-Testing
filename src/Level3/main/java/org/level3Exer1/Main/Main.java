cb491a20dc20e3a7c9f8208c5c6a1b2c17ee5bf7
isaac.diez <isaac.diez@proton.me>
2025-01-24 12:48:17 +0100

    feat: Level 3 Exer 1: Implement TDD for Calculadora class
    - Created a test class `TestCalculator` with unit tests for the `add`, `subtract`, `multiply`, and `divide` methods.
    - Executed the initial tests, which failed as expected since the `Calculator` class methods were not yet implemented.
    - Implemented the `Calculator` class with the required methods to pass the tests.
    - Ran the tests again to verify the implementation.
    - Iterated on the TDD process: refactored code, improved tests, and extended functionality as needed.

---
+++ src/Level3/main/java/org/level3Exer1/Main/Main.java
@@ -0,0 +1,19 @@
+package org.level3Exer1.Main;
+
+import org.level3Exer1.Model.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator hp = new Calculator();
+
+        System.out.println(hp.add(2.2,3.3));
+
+        System.out.println(hp.subtract(10.2,6.7));
+
+        System.out.println(hp.multiply(4.3,8.6));
+
+        System.out.println(hp.divide(20.11,3.14));
+
+    }
+}
---
+++ src/Level3/test/java/org/level3Exer1/Model/TestCalculator.java
@@ -0,0 +1,53 @@
+package org.level3Exer1.Model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+
+public class TestCalculator {
+
+    private Calculator casio;
+
+    @BeforeEach
+    public void initial() {
+
+        casio = new Calculator();
+    }
+
+    @Test
+    public void testSumOfTwoDoubleNumbers() {
+
+        Double sum = casio.add(3.0,5.5);
+
+        assertThat(sum).isEqualTo(8.5);
+    }
+
+    @Test
+    public void testSubtractionOfTwoDoubleNumbers() {
+
+        Double subtraction = casio.subtract(8.2,4.4);
+
+        assertThat(subtraction).isCloseTo(3.8, within(0.0001));
+    }
+
+    @Test
+    public void testMultiplicationOfTwoDoubleNumbers() {
+
+        Double multiply = casio.multiply(5.2,4.4);
+
+        assertThat(multiply).isCloseTo(22.88, within(0.0001));
+    }
+
+    @Test
+    public void testDivisionOfTwoDoubleNumbers() {
+
+        Double divide = casio.divide(12.2,2.5);
+
+        assertThat(divide).isCloseTo(4.88, within(0.0001));
+    }
+
+
+
+
+}
